471d5d84589936f8bcc448987ab065daa8c85fa0
Rafael <rafael10.rj@hotmail.com>
2015-08-05 10:47:17 -0300

    sobrescrito método equals

--- VeiculosLRV/src/br/com/veiculoslrv/veiculos/Veiculo.java
+++ VeiculosLRV/src/br/com/veiculoslrv/veiculos/Veiculo.java
@@ -10,6 +10,59 @@ public abstract class Veiculo {
 
 	
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((chassi == null) ? 0 : chassi.hashCode());
+		result = prime * result + ((cor == null) ? 0 : cor.hashCode());
+		result = prime * result + ((modelo == null) ? 0 : modelo.hashCode());
+		result = prime * result + ((montadora == null) ? 0 : montadora.hashCode());
+		result = prime * result + Float.floatToIntBits(preco);
+		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Veiculo other = (Veiculo) obj;
+		if (chassi == null) {
+			if (other.chassi != null)
+				return false;
+		} else if (!chassi.equals(other.chassi))
+			return false;
+		if (cor == null) {
+			if (other.cor != null)
+				return false;
+		} else if (!cor.equals(other.cor))
+			return false;
+		if (modelo == null) {
+			if (other.modelo != null)
+				return false;
+		} else if (!modelo.equals(other.modelo))
+			return false;
+		if (montadora == null) {
+			if (other.montadora != null)
+				return false;
+		} else if (!montadora.equals(other.montadora))
+			return false;
+		if (Float.floatToIntBits(preco) != Float.floatToIntBits(other.preco))
+			return false;
+		if (tipo == null) {
+			if (other.tipo != null)
+				return false;
+		} else if (!tipo.equals(other.tipo))
+			return false;
+		return true;
+	}
+	
+
 	public String getChassi() {
 		return chassi;
 	}
