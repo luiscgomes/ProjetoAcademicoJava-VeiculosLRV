3e432efc7fbea7c60ca6774433512e6355ee883e
rafael.cunha <rafael.cunha@al.infnet.edu.br>
2015-08-10 15:31:15 -0300

    Refatorado o teste de unidade

--- VeiculosLRV/src/br/com/veiculoslrv/teste/LojaTest.java
+++ VeiculosLRV/src/br/com/veiculoslrv/teste/LojaTest.java
@@ -1,6 +1,6 @@
 package br.com.veiculoslrv.teste;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,24 +11,27 @@ import br.com.veiculoslrv.veiculos.Motocicleta;
 
 public class LojaTest {
 	private Loja loja;
+	private Carro carro;
+	private Motocicleta moto;
 
 	@Before
 	public void criaLoja() {
 		this.loja = new Loja();
 		loja.setNome("VeiculosLRV");
 		loja.setEndereco("Rua Centro 1263, 21775-846, Rio de janeiro");
+
+		this.carro = new Carro();
+		carro.setModelo("HB20");
+		carro.setChassi("abc123");
+
+		moto = new Motocicleta();
+		moto.setModelo("BIZ 120");
+		moto.setChassi("MT120");
+
 	}
 
 	@Test
 	public void pesquisarVeiculoPorChassi() {
-
-		Carro carro = new Carro();
-		carro.setModelo("HB20");
-		carro.setChassi("abc123");
-
-		Motocicleta moto = new Motocicleta();
-		moto.setModelo("BIZ 120");
-		moto.setChassi("MT120");
 
 		loja.AdicionarVeiculo(carro);
 		loja.AdicionarVeiculo(moto);
@@ -40,12 +43,7 @@ public class LojaTest {
 
 	@Test(expected = IllegalArgumentException.class)
 	public void naoDeveAceitarPrecoNegativo() {
-
-		Carro carro = new Carro();
-		carro.setModelo("HB20");
-		carro.setChassi("abc123");
 		carro.setPreco(-100);
-
 		loja.AdicionarVeiculo(carro);
 
 	}
