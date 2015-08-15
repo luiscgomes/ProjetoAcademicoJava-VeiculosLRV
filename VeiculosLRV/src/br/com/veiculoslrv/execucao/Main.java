cd30e58df71df65b1eb68ee32b2262a576a5c823
Rafael <rafael10.rj@hotmail.com>
2015-08-15 20:55:30 -0300

    Refatodado o menu

--- VeiculosLRV/src/br/com/veiculoslrv/execucao/Main.java
+++ VeiculosLRV/src/br/com/veiculoslrv/execucao/Main.java
@@ -4,6 +4,9 @@ import br.com.veiculoslrv.loja.Menu;
 
 public class Main {
 	public static void main(String[] args) {
-		Menu.exibir();
+		int opcao;
+
+		Menu.exibirPrincipal();
+
 	}
 }
--- VeiculosLRV/src/br/com/veiculoslrv/loja/Menu.java
+++ VeiculosLRV/src/br/com/veiculoslrv/loja/Menu.java
@@ -2,35 +2,59 @@ package br.com.veiculoslrv.loja;
 
 import java.util.Scanner;
 
+import br.com.veiculoslrv.veiculos.Carro;
+import br.com.veiculoslrv.veiculos.Veiculo;
+
 public final class Menu {
-	public static void exibir() {
+	public static int exibirPrincipal() {
 		Scanner ler = new Scanner(System.in);
-		int opcao;
 
-		do {
-			System.out.printf("**** Menu Principal *****\n");
-			System.out.printf("[ 1 ] Adicionar Carro\n");
-			System.out.printf("[ 2 ] Adicionar Moto\n");
-			System.out.printf("[ 3 ] Pesquisar Carro\n");
-			System.out.printf("[ 4 ] Pesquisar Moto\n");
-			System.out.printf("[ 5 ] Buscar Veiculo por Chassi\n");
-			System.out.printf("[ 6 ] Listar Estoque de Carros\n");
-			System.out.printf("[ 7 ] Listar Estoque de Motos\n");
-			System.out.printf("[ 0 ] Encerrar o Programa\n");
-			System.out.printf("\nOpção Desejada: ");
-
-			opcao = ler.nextInt();
-
-			switch (opcao) {
-			case 1:
-				break;
-
-			default:
-				System.out.println("Opção invalida");
-
-			}
-			System.out.printf("\n\n");
-		} while (opcao != 0);
+		System.out.println("**** Menu Principal *****");
+		System.out.println("[ 1 ] Adicionar Carro");
+		System.out.println("[ 2 ] Adicionar Moto");
+		System.out.println("[ 3 ] Pesquisar Carro");
+		System.out.println("[ 4 ] Pesquisar Moto");
+		System.out.println("[ 5 ] Buscar Veiculo por Chassi");
+		System.out.println("[ 6 ] Listar Estoque de Carros");
+		System.out.println("[ 7 ] Listar Estoque de Motos");
+		System.out.println("[ 0 ] Encerrar o Programa");
+		System.out.println("\nOpção Desejada: ");
+		return ler.nextInt();
 
 	}
+
+	public static Carro adicionarCarro() {
+		Carro carro = new Carro();
+		Scanner ler = new Scanner(System.in);
+
+		System.out.println("**** Adicionar Carro *****");
+		System.out.println("Informa o Chassi: ");
+		carro.chassi = ler.nextLine();
+
+		System.out.println("Informa a Montadora: ");
+		carro.montadora = ler.nextLine();
+
+		System.out.println("Informa o Modelo: ");
+		carro.modelo = ler.nextLine();
+
+		System.out.println("Informa o Tipo: ");
+		carro.tipo = ler.nextLine();
+
+		System.out.println("Informa a Por: ");
+		carro.cor = ler.nextLine();
+
+		System.out.println("Informa o Preco: ");
+		carro.preco = ler.nextFloat();
+
+		System.out.println("Informa a Motorizacao: ");
+		carro.preco = ler.nextFloat();
+
+		System.out.println("Informa o Cambio: ");
+		carro.cor = ler.nextLine();
+		return carro;
+
+	}
+	
+	
+
 }
