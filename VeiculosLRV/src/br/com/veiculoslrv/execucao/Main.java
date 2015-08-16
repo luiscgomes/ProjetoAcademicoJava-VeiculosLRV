23f69dc71e16eb85bbc02a0c6d137f4b71c2022c
Rafael <rafael10.rj@hotmail.com>
2015-08-16 01:13:56 -0300

    Refatorado

--- VeiculosLRV/src/br/com/veiculoslrv/execucao/Main.java
+++ VeiculosLRV/src/br/com/veiculoslrv/execucao/Main.java
@@ -5,8 +5,7 @@ import br.com.veiculoslrv.loja.Menu;
 
 public class Main {
 	public static void main(String[] args) {
-		Loja loja = new Loja("VeiculosLRV", "Rua do ouro 21, 23775-594, Rio de Janeiro, Brasil");
-		
+		Loja loja = new Loja("VeiculosLRV", "Rua do ouro 21, 23775-594, Rio de Janeiro, Brasil");		
 		int opcao;
 
 		do {
@@ -17,7 +16,22 @@ public class Main {
 				break;
 			case 2:
 				loja.AdicionarVeiculo(Menu.adicionarMoto());
-
+				break;
+			case 3:
+				//Buscar Carro
+				break;
+			case 4:
+				//Buscar Moto
+				break;
+			case 5:
+				//Buscar Veiculo por Chassi
+				break;
+			case 6:
+				//Listar Estoque de Carros
+				break;
+			case 7:
+				//Listar Estoque de moto
+				break;
 			default:
 				break;
 			}
