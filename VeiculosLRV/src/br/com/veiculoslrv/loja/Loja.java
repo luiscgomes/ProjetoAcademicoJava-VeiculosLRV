b61ce89a74984eb39ad91c5b5eb99c33c85aa26f
Luis Gomes <luis.csg96@gmail.com>
2015-09-13 18:50:39 -0300

    Acrescendo o método para manipular o estoque no arquivo

--- VeiculosLRV/src/br/com/veiculoslrv/loja/Loja.java
+++ VeiculosLRV/src/br/com/veiculoslrv/loja/Loja.java
@@ -1,7 +1,9 @@
 package br.com.veiculoslrv.loja;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import br.com.veiculoslrv.util.ArquivoUtil;
 import br.com.veiculoslrv.veiculos.Especificacao;
 import br.com.veiculoslrv.veiculos.TipoVeiculo;
 import br.com.veiculoslrv.veiculos.Veiculo;
@@ -9,8 +11,8 @@ import br.com.veiculoslrv.veiculos.Veiculo;
 public class Loja {
 	private String endereco;
 	private String nome;
-	private ArrayList<Veiculo> estoqueVeiculos = new ArrayList<Veiculo>();
-	
+	private List<Veiculo> estoqueVeiculos = new ArrayList<Veiculo>();	
+	private static String NOME_ARQUIVO_ESTOQUE = "estoque.txt";
 	
 	public Loja(String nome, String endereco) {
 		super();
@@ -25,6 +27,24 @@ public class Loja {
 	public Veiculo pesquisarVeiculo(Especificacao especificacao) {
 		for (Veiculo veiculo : estoqueVeiculos) {
 			if (veiculo.getEspecificacao().equals(especificacao)) {
+				return veiculo;
+			}
+		}
+		
+		return null;
+	}
+	
+	public Veiculo buscarCarro(String chassi) {
+		return buscarVeiculo(chassi, TipoVeiculo.carro);
+	}
+	
+	public Veiculo buscarMoto(String chassi) {
+		return buscarVeiculo(chassi, TipoVeiculo.motocicleta);
+	}
+	
+	private Veiculo buscarVeiculo(String chassi, TipoVeiculo tipoVeiculo) {
+		for (Veiculo veiculo : estoqueVeiculos) {
+			if ((veiculo.getChassi() == chassi) && (veiculo.getEspecificacao().getTipoVeiculo() == tipoVeiculo)) {
 				return veiculo;
 			}
 		}
@@ -62,6 +82,14 @@ public class Loja {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public void salvarEstoque() {
+		ArquivoUtil.salvaDados(NOME_ARQUIVO_ESTOQUE, estoqueVeiculos);
+	}
+	
+	public void recuperarEstoque() {
+		estoqueVeiculos = ArquivoUtil.recuperaDados(NOME_ARQUIVO_ESTOQUE);
 	}
 
 }
