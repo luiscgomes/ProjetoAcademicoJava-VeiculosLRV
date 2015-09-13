2f147f2d5a2c487dc4a93c55124cbe22a0684c8d
Luis Gomes <luis.csg96@gmail.com>
2015-09-13 18:50:05 -0300

    Alterado a fábrica de veículo para retornar o veículo pelo seu tipo

--- VeiculosLRV/src/br/com/veiculoslrv/fabrica/FabricaVeiculo.java
+++ VeiculosLRV/src/br/com/veiculoslrv/fabrica/FabricaVeiculo.java
@@ -1,18 +1,17 @@
 package br.com.veiculoslrv.fabrica;
 
+import br.com.veiculoslrv.veiculos.Especificacao;
 import br.com.veiculoslrv.veiculos.TipoVeiculo;
 import br.com.veiculoslrv.veiculos.Veiculo;
 
 public class FabricaVeiculo {
 
-	public Veiculo criarVeiculo(TipoVeiculo veiculo) {
-		switch (veiculo) {
-		case carro:
-			return new Carro();
-		case motocicleta:
-			return new Moto();
-		default:
-			throw new IllegalArgumentException("Tipo Invalido");
-		}
+	public Veiculo criarVeiculo(TipoVeiculo tipoVeiculo) {
+		Veiculo veiculo = new Veiculo();
+		Especificacao especificacao = new Especificacao();
+		
+		especificacao.setTipoVeiculo(tipoVeiculo);
+		
+		return veiculo;
 	}
 }
