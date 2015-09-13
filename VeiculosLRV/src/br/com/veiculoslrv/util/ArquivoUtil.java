2634d40f386b3870a78fa8357e6933c8f8a8b5f4
Luis Gomes <luis.csg96@gmail.com>
2015-09-13 18:52:06 -0300

    Criado o utilitário de manipulador de arquivo de texto

---
+++ VeiculosLRV/src/br/com/veiculoslrv/util/ArquivoUtil.java
@@ -0,0 +1,72 @@
+package br.com.veiculoslrv.util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.veiculoslrv.veiculos.Veiculo;
+
+public class ArquivoUtil {
+	// Salva uma lista de objetos de um arquivo de texto
+	public static void salvaDados(String nomeArquivo, List<Veiculo> veiculos) {
+		ObjectOutputStream objectOutputStream = null;
+		try {
+			objectOutputStream = new ObjectOutputStream(new FileOutputStream(nomeArquivo));
+			
+			objectOutputStream.writeObject(veiculos);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (objectOutputStream != null) {
+					objectOutputStream.flush();
+					objectOutputStream.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}				
+	}	
+	
+	// Recupera um lista de objetos de um arquivo de texto
+	public static List<Veiculo> recuperaDados(String nomeArquivo) {
+		ObjectInputStream objectInputStream = null;
+		
+		List<Veiculo> veiculos = new ArrayList<Veiculo>();
+		
+		try {
+			objectInputStream = new ObjectInputStream(new FileInputStream(nomeArquivo));
+			
+			try {
+				List<Veiculo> readObject = (List<Veiculo>) objectInputStream.readObject();
+				veiculos = readObject;				 
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (objectInputStream != null) {					
+					objectInputStream.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return veiculos;
+	}
+}
