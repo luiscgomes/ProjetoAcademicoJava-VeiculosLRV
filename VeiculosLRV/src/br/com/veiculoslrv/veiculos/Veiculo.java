f326f629dc5b3f285d721acf14699b648aea44ca
Luis Gomes <luis.csg96@gmail.com>
2015-09-20 22:47:03 -0300

    Sobrescrito o equals e o hash map

--- VeiculosLRV/src/br/com/veiculoslrv/veiculos/Veiculo.java
+++ VeiculosLRV/src/br/com/veiculoslrv/veiculos/Veiculo.java
@@ -82,4 +82,29 @@ public class Veiculo implements Serializable {
 
 		this.preco = preco;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((chassi == null) ? 0 : chassi.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Veiculo other = (Veiculo) obj;
+		if (chassi == null) {
+			if (other.chassi != null)
+				return false;
+		} else if (!chassi.equals(other.chassi))
+			return false;
+		return true;
+	}
 }
