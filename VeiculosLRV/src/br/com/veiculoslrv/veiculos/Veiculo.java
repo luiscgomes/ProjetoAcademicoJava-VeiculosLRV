ce441f5b450cd853a905baa60f1edcef3375d4b5
Luis Gomes <luis.csg96@gmail.com>
2015-09-20 22:48:20 -0300

    Sobrescrito o equals

--- VeiculosLRV/src/br/com/veiculoslrv/veiculos/Veiculo.java
+++ VeiculosLRV/src/br/com/veiculoslrv/veiculos/Veiculo.java
@@ -84,14 +84,6 @@ public class Veiculo implements Serializable {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((chassi == null) ? 0 : chassi.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
